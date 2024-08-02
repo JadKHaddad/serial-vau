b89fa4ae57937bdbb1ce50f1a91a328ee48caf05
JadKHaddad <jadkhaddad@gmail.com>
2024-08-02 17:37:04 +0200

    added system theme detection
    Signed-off-by: JadKHaddad <jadkhaddad@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -7,5 +7,26 @@
 </template>
 
 <script lang="ts" setup>
-  //
+import { onMounted, onUnmounted } from 'vue';
+import { listen, UnlistenFn } from '@tauri-apps/api/event';
+import { useTheme } from 'vuetify'
+
+const theme = useTheme()
+
+let unlistenThemeChangedEvent: UnlistenFn;
+
+onMounted(async () => {
+  unlistenThemeChangedEvent = await listen('tauri://theme-changed', (event) => {
+    const themeName = event.payload as string;
+    if (themeName === 'dark' || themeName === 'light') {
+      theme.global.name.value = themeName;
+    }
+  });
+});
+
+onUnmounted(() => {
+  if (unlistenThemeChangedEvent) {
+    unlistenThemeChangedEvent();
+  }
+});
 </script>
