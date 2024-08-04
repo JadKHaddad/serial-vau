28204bec67a2463d3abf25b4b4a16d85e13f747c
JadKHaddad <jadkhaddad@gmail.com>
2024-08-04 21:12:08 +0200

    feat/side-nav: navigation-drawer: highlight active route
    Signed-off-by: JadKHaddad <jadkhaddad@gmail.com>

--- src/components/AppDrawer.vue
+++ src/components/AppDrawer.vue
@@ -1,14 +1,25 @@
 <template>
     <v-navigation-drawer rail permanent app>
         <v-list density="compact" nav>
-            <router-link to="/" style="text-decoration: none; color: inherit;">
-                <v-list-item prepend-icon="mdi-serial-port" value="serial-port"></v-list-item>
+            <router-link to="/" style="text-decoration: none; color: inherit;" active-class="v-list-item--active">
+                <v-list-item :class="isActiveRoute('/') ? 'v-list-item--active' : ''" prepend-icon="mdi-serial-port"
+                    value="serial-port"></v-list-item>
             </router-link>
-
-            <router-link to="/monitor" style="text-decoration: none; color: inherit;">
-                <v-list-item prepend-icon="mdi-monitor" value="serial-monitor"></v-list-item>
+            <router-link to="/monitor" style="text-decoration: none; color: inherit;"
+                active-class="v-list-item--active">
+                <v-list-item :class="isActiveRoute('/monitor') ? 'v-list-item--active' : ''" prepend-icon="mdi-monitor"
+                    value="serial-monitor"></v-list-item>
             </router-link>
-
         </v-list>
     </v-navigation-drawer>
 </template>
+
+<script lang="ts" setup>
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const isActiveRoute = (path: string) => {
+    return route.path === path;
+};
+</script>
