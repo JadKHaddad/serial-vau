4a072666101274c23fefde5dc857c9b7b3a023ab
jha <jad.haddad@gselectronic.com>
2024-08-08 11:34:46 +0200

    feat/serial-monitor-screen: docs: stores::app
    Signed-off-by: jha <jad.haddad@gselectronic.com>

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -1,4 +1,8 @@
-// Utilities
+// TODO: make every function to the backend (tauri or any other) that required user interaction return a value and set it here in the sate.
+// Events should only be processed, when something in the backend happened without user interaction.
+// TODO: Move the functions and the event listeners to a single module. move the backend specific functions and events to a backend specific module
+// then use the backend specific module in the Functions/Events module.
+// this will allow us to handle seperate backends (taurim or web) (web events are socketio events).
 import { PacketData } from '@/models/intern/packet-data';
 import { ManagedSerialPort } from '@/models/managed-serial-port';
 import { defineStore } from 'pinia'
