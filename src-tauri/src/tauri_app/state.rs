5e806a8203640d22a79c8fc1fbae2a67b04eec92
JadKHaddad <jadkhaddad@gmail.com>
2024-08-11 14:36:54 +0200

    feat/tauri-app-module: TauriAppState does not have an inner Arc
    Signed-off-by: JadKHaddad <jadkhaddad@gmail.com>

--- src-tauri/src/tauri_app/state.rs
+++ src-tauri/src/tauri_app/state.rs
@@ -1,39 +1,20 @@
-use std::{ops::Deref, sync::Arc};
-
 use crate::app::state::State as AppState;
 use crate::core::state::State as SerialState;
 
-#[derive(Debug, Clone, Default)]
+#[derive(Debug, Default)]
 pub struct State {
-    inner: Arc<StateInner>,
-}
-
-impl Deref for State {
-    type Target = StateInner;
-
-    fn deref(&self) -> &Self::Target {
-        &self.inner
-    }
+    serial_state: SerialState,
+    app_state: AppState,
 }
 
 impl State {
     pub fn new(serial_state: SerialState, app_state: AppState) -> Self {
         Self {
-            inner: Arc::new(StateInner {
-                serial_state,
-                app_state,
-            }),
+            serial_state,
+            app_state,
         }
     }
-}
 
-#[derive(Debug, Default)]
-pub struct StateInner {
-    serial_state: SerialState,
-    app_state: AppState,
-}
-
-impl StateInner {
     pub fn serial_state(&self) -> &SerialState {
         &self.serial_state
     }
