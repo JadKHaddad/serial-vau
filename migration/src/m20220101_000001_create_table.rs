55d6eb55fa1f8fd83625336629f949d7073286ad
Jad K. Haddad <jadkhaddad@gmail.com>
2024-09-30 15:06:15 +0200

    feat(database): added SerialPort table and indecies
    Signed-off-by: Jad K. Haddad <jadkhaddad@gmail.com>

--- migration/src/m20220101_000001_create_table.rs
+++ migration/src/m20220101_000001_create_table.rs
@@ -10,6 +10,39 @@ impl MigrationTrait for Migration {
         manager
             .create_table(
                 Table::create()
+                    .table(SerialPort::Table)
+                    .col(
+                        ColumnDef::new(SerialPort::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(
+                        ColumnDef::new(SerialPort::Name)
+                            .char_len(32)
+                            .not_null()
+                            .unique_key(),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+
+        // Index on Name column
+        manager
+            .create_index(
+                Index::create()
+                    .table(SerialPort::Table)
+                    .col(SerialPort::Name)
+                    .name("idx_serial_port_name")
+                    .unique()
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .create_table(
+                Table::create()
                     .table(OpenOptions::Table)
                     .col(
                         ColumnDef::new(OpenOptions::Id)
@@ -18,7 +51,12 @@ impl MigrationTrait for Migration {
                             .auto_increment()
                             .primary_key(),
                     )
-                    .col(ColumnDef::new(OpenOptions::Tag).text().not_null())
+                    .col(
+                        ColumnDef::new(OpenOptions::SerialPortId)
+                            .integer()
+                            .not_null(),
+                    )
+                    .col(ColumnDef::new(OpenOptions::Tag).char_len(32).not_null())
                     .col(enumeration(
                         OpenOptions::InitReadState,
                         Alias::new("init_read_state"),
@@ -50,15 +88,38 @@ impl MigrationTrait for Migration {
                             .integer()
                             .not_null(),
                     )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .on_update(ForeignKeyAction::Cascade)
+                            .on_delete(ForeignKeyAction::Cascade)
+                            .from(OpenOptions::Table, OpenOptions::SerialPortId)
+                            .to(SerialPort::Table, SerialPort::Id),
+                    )
                     .to_owned(),
             )
             .await?;
+
+        // Index on Tag column
+        manager
+            .create_index(
+                Index::create()
+                    .table(OpenOptions::Table)
+                    .col(OpenOptions::Tag)
+                    .name("idx_open_options_tag")
+                    .to_owned(),
+            )
+            .await?;
+
         Ok(())
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         manager
             .drop_table(Table::drop().table(OpenOptions::Table).to_owned())
+            .await?;
+
+        manager
+            .drop_table(Table::drop().table(SerialPort::Table).to_owned())
             .await?;
 
         Ok(())
@@ -103,6 +164,7 @@ pub enum StopBits {
 enum OpenOptions {
     Table,
     Id,
+    SerialPortId,
     Tag,
     InitReadState,
     BaudRate,
@@ -111,4 +173,11 @@ enum OpenOptions {
     Parity,
     StopBits,
     TimeoutMilliSecs,
+}
+
+#[derive(DeriveIden)]
+enum SerialPort {
+    Table,
+    Id,
+    Name,
 }
