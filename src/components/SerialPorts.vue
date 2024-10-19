896f42b212f58ca92a60d8832cdd558b6297492a
farbian_2005 <fabian_bernhart@web.de>
2024-10-19 15:17:07 +0200

    fix: improve responsiveness Serial Ports

--- src/components/SerialPorts.vue
+++ src/components/SerialPorts.vue
@@ -1,54 +1,47 @@
 <template>
-  <v-container class="fill-height d-colum flex-column pa-sm--0">
+  <v-container fluid style="height: calc(100%)">
     <h1 class="text-h3 text-center mb-6">Serial Ports</h1>
-    <v-row scrollable justify="start">
-      <v-col 
-        v-for="(port, index) in app.managedSerialPorts"
-        :key="index"
-        cols="12"
-        md="6"
-      >
-        <v-card variant="outlined">
-          <SerialPort :port="port"></SerialPort>
-        </v-card>
+
+    <v-responsive class="pa-5 scrollable-cards" style="height: calc(83%)">
+      <v-row>
+        <v-col
+          v-for="(port, index) in app.managedSerialPorts"
+          :key="index"
+          cols="12"
+          md="4"
+        >
+          <v-lazy
+            :options="{
+              threshold: 0.5,
+            }"
+          >
+            <v-card
+              variant="outlined"
+              class="pa-4 fill-height d-flex flex-column"
+            >
+              <SerialPort :port="port"></SerialPort>
+            </v-card>
+          </v-lazy>
+        </v-col>
+      </v-row>
+    </v-responsive>
+
+    <v-row class="pa-2" style="height: 4%">
+      <v-col cols="12">
+        <v-text-field
+          v-model="broadcastMessage"
+          class="mx-16"
+          label="Input text here"
+          variant="outlined"
+          density="comfortable"
+          append-icon="mdi-send"
+          @keydown.enter.prevent="sendBroadcastMessage()"
+          @click:append="sendBroadcastMessage"
+          @click:clear="clearBroadcastMessage"
+          clearable
+        ></v-text-field>
       </v-col>
     </v-row>
-
-    <v-container fluid>
-      <v-sheet
-        color="transparent"
-        class="d-flex justify-center align-center"
-        max-width="600"
-        style="margin: auto"
-      >
-        <v-text-field
-          autofocus
-          messages="Okay then"
-          hide-details="auto"
-          max-width="500"
-          placeholder="example: lights off"
-          variant="outlined"
-          class="text-none"
-          v-model="broadcastValue"
-          label="Send Broadcast Message"
-          append-icon="mdi-send"
-          @keydown.enter.prevent="sendBroadCastMessageToAllPorts"
-          @click:append="sendBroadCastMessageToAllPorts"
-          @click:clear="clearBroadcastValue"
-          clearable
-        >
-          <template v-slot:message>
-            <span class="hidden-sm-and-down">
-              Serial Vau can have some errors. Please report immediately (*o*)
-            </span>
-
-            <span class="hidden-md-and-up">
-              Please report errors immediately (*o*)
-            </span>
-          </template>
-        </v-text-field>
-      </v-sheet>
-    </v-container>
   </v-container>
 </template>
 
@@ -58,14 +51,23 @@ import { useAppStore } from "@/stores/app";
 
 const app = useAppStore();
 
-const broadcastValue = ref<string>("");
+const broadcastMessage = ref("");
 
-const clearBroadcastValue = () => {
-  broadcastValue.value = "";
+const clearBroadcastMessage = () => {
+  broadcastMessage.value = "";
 };
 
-const sendBroadCastMessageToAllPorts = () => {
-  app.sendToAllSerialPorts(broadcastValue.value);
-  clearBroadcastValue();
+const sendBroadcastMessage = () => {
+  if (broadcastMessage.value) {
+    app.sendToAllSerialPorts(broadcastMessage.value);
+    broadcastMessage.value = "";
+  }
 };
 </script>
+
+<style scoped>
+.scrollable-cards {
+  overflow-y: auto; /* Enable vertical scrolling */
+  padding: 10px;
+}
+</style>
